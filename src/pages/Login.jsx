452906397f113910da511b788f0c07c07a46e1c7
refentse1 <95753720+refentse1@users.noreply.github.com>
2023-08-20 18:38:02 +0200

    Revert "Additional frontend features"

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,77 +3,23 @@ import { useNavigate, Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+
 import logo from "../assets/logo.png";
-import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import "./styles.css";
 import { AppContext } from "../context/AppContext";
 
 const Login = () => {
+  // const navigate = useNavigate();
 
-  const navigate = useNavigate();
-  const isTabletOrLaptop = useMediaQuery({ query: '(min-width: 768px)' });
-  const [passwordVisible, setPasswordVisible] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
-  const [isErrorVisible, setIsErrorVisible] = useState(false);
-
-  const togglePasswordVisibility = () => {
-    setPasswordVisible(!passwordVisible);
-  };
-
-  const [inputValue, setInputValue] = useState({
-    email: "",
-    password: "",
-  });
-
-  const { email, password } = inputValue;
-
-  const handleOnChange = (e) => {
-    const { name, value } = e.target;
-    setInputValue({
-      ...inputValue,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setIsLoading(true);
-    try {
-      await new Promise(resolve => setTimeout(resolve, 1500));
-      fetch("http://localhost:3001/users/login", {
-        method: 'POST',
-        headers: {
-          'Content-Type':'application/json'
-        },
-        body: JSON.stringify(inputValue),
-      }).then((response) => {
-
-        if(!response.ok) {
-          response.json().then(data => {
-            console.log(data.message);
-            setErrorMessage(data.message);
-            setIsErrorVisible(true);
-          });
-        }
-        else {
-          console.log("User logged in successfully");
-          navigate('/dashboard/dashboard/dashboard');
-        }
-
-      })
-
-    } catch (error) {
-      console.log(error);
-    }
-    setInputValue({
-      ...inputValue,
-      email: "",
-      password: "",
-    });
-    console.log(isLoading);
-    setIsLoading(false);
-  };
+  const {
+    isTabletOrLaptop,
+    passwordVisible,
+    email,
+    password,
+    togglePasswordVisibility,
+    LoginOnChange,
+    LoginSubmit,
+  } = useContext(AppContext);
 
   return (
     <motion.div
@@ -121,25 +67,15 @@ const Login = () => {
               className="eye-icon"
             />
 
-            {isErrorVisible && (
-              <div className="error-message">
-                <span>{errorMessage}</span>
-                <button className="close-button" onClick={() => setIsErrorVisible(false)}>
-                  &#x2716; {/* Unicode character for '✖' */}
-                </button>
-              </div>
-            )}
-
             <Link to="/resetpassword" className="forgot-password-link">
               Forgot Password?
             </Link>
 
             <button
               className="login-button"
-              style={{ width: isTabletOrLaptop ? '15rem' : '50%' }}
-              disabled={isLoading}
+              style={{ width: isTabletOrLaptop ? "15rem" : "50%" }}
             >
-              {isLoading ? <FontAwesomeIcon icon={faSpinner} spin /> : 'Login'}
+              Login
             </button>
 
             <Link to="/signup" className="signup-link">
