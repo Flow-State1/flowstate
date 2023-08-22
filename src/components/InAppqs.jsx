c62ffba50dae4bddc7a3d5b6cf82686c46a167f4
KatlehoMadaba <katlehomadaba44@gmail.com>
2023-08-22 22:29:58 +0200

    "with c error"

--- src/components/InAppqs.jsx
+++ src/components/InAppqs.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import { useState,useEffect } from 'react';
 import "./question.css";
-const InAppqs=(props)=>{
-    useEffect(()=>{
-        fetch('https://jsonplaceholder.typicode.com/todos/1').then(res.json()).then(data=>console.log(data))
-    },[])
-    return(
-        <div className="a-questions">        
-            Hello world
-        </div>
 
-        )
+const InAppqs=(props)=>{
+    const {setState}=props
+    useEffect(()=>{
+        fetch("https://jsonplaceholder.typicode.com/todos")
+        .then((res)=>res.json())
+        .then((data)=>{
+            const fiveFirstTodos=data.slice(0,6);
+            setState((state)=>({...state,inAppqs:fiveFirstTodos}))
+        });
+    },[]);
+    renderInAppsqs=()=>{
+        props.inAppqs.map(inAppq=>{
+        <li className="q-widgets" key={inAppqs.id}></li>
+        })
+    }
+    return(
+        <div className="a-questions">   
+        <ul className="q-widgets">
+        {this.renderInAppsqs()}
+        </ul>  
+        </div>
+        )  
 }
 export default InAppqs
